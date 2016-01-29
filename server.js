ae9471937543d3d0bfb37866ea39652695ad59a8
Carine TOdmia <ctodmia@gmail.com>
2016-01-28 19:33:54 -0800

    testing git request, adding config.js to gitignore

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('we got this');
+	console.log('this is req.body', req.body)
+})
+
+app.listen('8080');
+console.log('it is currently going down on port 8080');
